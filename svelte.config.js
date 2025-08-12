931350eb30fbec6740ef491f651c2a58f41952ec
oggunderscore <oggunderscore@gmail.com>
2025-08-11 22:43:40 -0700

    Attempt at a fix?

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from "@sveltejs/adapter-auto";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // Try to enable fallback for SPA mode
-      fallback: 'index.html'
-    }),
+    adapter: adapter(),
     paths: {
       base: "",
     },
